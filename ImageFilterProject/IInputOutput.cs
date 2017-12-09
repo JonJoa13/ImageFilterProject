b6fd667650b3923687c324144a1bdd705cc20bbb
vcaibis <vcaibis@gmail.com>
2017-12-09 11:04:48 +0100

    Changement nom interfaces

--- ImageFilterProject/IInputOutput.cs
+++ ImageFilterProject/IInputOutput.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ImageFilterProject
 {
-    interface InterfaceInputOutput
+    interface IInputOutput
     {
     }
 }
--- ImageFilterProject/Matrix.cs
+++ ImageFilterProject/Matrix.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ImageFilterProject
 {
-    interface InterfaceGUI
+    class Matrix
     {
     }
 }
